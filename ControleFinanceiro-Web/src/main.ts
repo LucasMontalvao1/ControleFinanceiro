4ba5da3cea48d3fed9166eb107d1e151e410f3a6
MAXIMA\lucas.montalvao <lucas.montalvao@maximatech.com.br>
2026-02-03 21:49:15 -0300

    debug: adiciona logs para investigar tela branca

--- ControleFinanceiro-Web/src/main.ts
+++ ControleFinanceiro-Web/src/main.ts
@@ -3,5 +3,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
+console.log('Checking Angular Bootstrap...');
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
